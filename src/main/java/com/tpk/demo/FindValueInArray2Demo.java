5cc83755a6feba7ffdb3004f3918d8943dabc3e2
tpk <tianpanke@ilarge.cn>
2020-03-19 10:43:20 +0800

    Initial commit

---
+++ src/main/java/com/tpk/demo/FindValueInArray2Demo.java
@@ -0,0 +1,21 @@
+package com.tpk.demo;
+
+/**
+ * @program: offer
+ * @description: 二维数组排序
+ * @author: tianpanke
+ * @create: 2020-03-19
+ **/
+public class FindValueInArray2Demo {
+
+    /**
+     * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
+     * 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
+     * 判断数组中是否含有该整数。
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/tpk/demo/readme.java
@@ -0,0 +1,10 @@
+package com.tpk.demo;
+
+/**
+ * @program: offer
+ * @description:
+ * @author: tianpanke
+ * @create: 2020-03-19
+ **/
+public class readme {
+}
