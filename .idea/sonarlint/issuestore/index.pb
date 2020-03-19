b8afade459818c37b85ddfa94264899218c0d680
tpk <tianpanke@ilarge.cn>
2020-03-19 11:04:27 +0800

    ''

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+V
+&src/main/java/com/tpk/demo/readme.java,d\b\db59d5f3c57b3e384c4647ce655aea8c70fe0b6c
+e
+5src/main/java/com/tpk/demo/FindValueInArray2Demo.java,2\d\2d5c4ea07099163a5450a7ed13458695fe5b8437
--- src/main/java/com/tpk/demo/FindValueInArray2Demo.java
+++ src/main/java/com/tpk/demo/FindValueInArray2Demo.java
@@ -14,8 +14,40 @@ public class FindValueInArray2Demo {
      * 判断数组中是否含有该整数。
      */
 
-    public static void main(String[] args) {
+    // 如果你会写个简单的嵌套循环，那么该问题的解决也一定游刃有余，但关键点是如何做到最优解。
+    // 采用常规遍历方法解决，可以达到目的，但是整个算犯法的时间复杂度O(i*j);
+    // 认真研读题目，你会发现题目中给出的二维数组是有严格的递增规律的。将整个二维数组的展开图
+    // 铺到脑海里，你会发现左下角的数据key,往上走比之小，往左走比之小。可在该规律的基础上，利用
+    // 单层循环，判断目标值和key值大小，从而控制指针移动或成功退出。该算法时间复杂度O(i+j).
 
+    public static void main(String[] args) {
+        FindValueInArray2Demo demo=new FindValueInArray2Demo();
+        System.out.println(demo.find(6,demo.initArrayData()));
+    }
+    public int[][] initArrayData(){
+        int [][]array={{1,2,3},{2,4,6},{5,7,9}};
+        return array;
+    }
+    public boolean find(int target,int [][]array){
+        if(array==null||array.length==0||array[0].length==0){
+            return false;
+        }
+        int width=array[0].length;
+        int height=array.length;
+
+        int i=height-1;
+        int j=0;
+        while(i>=0&&j<width){
+            int value=array[i][j];
+            if(value==target){
+                return true;
+            }else if(value>target){
+                i--;
+            }else{
+                j++;
+            }
+        }
+        return false;
     }
 
 }
